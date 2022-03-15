a97a9046bd9303f1e9336bcb6255f8002613ff4a
zeprincelini <princelini4@gmail.com>
2022-03-15 23:53:33 +0100

    updated http requests url

--- http-requests/api.js
+++ http-requests/api.js
@@ -1,6 +1,8 @@
 const dev = process.env.NODE_ENV === "development";
 
-const url = dev ? "http://localhost:5000/api" : "";
+const url = dev
+  ? "http://localhost:5000/api"
+  : "https://bonds-backend-project.herokuapp.com/api";
 
 export const Sign_In = url + "/auth/login";
 export const Sign_Up = url + "/auth/register";
